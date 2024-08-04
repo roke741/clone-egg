ac0477ab70335698b24e5cdc48ff23ff55ef2ec4
roke741 <jordieroque741@gmail.com>
2024-08-04 01:09:28 -0500

    chore: Update Content component to render rich text with embedded assets and links

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,43 +1,86 @@
 import React, { useEffect, useState } from 'react';
-//import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+//import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { BLOCKS, INLINES, MARKS} from "@contentful/rich-text-types";
+
+
+const renderOptions = {
+  renderNode: {
+    [INLINES.EMBEDDED_ENTRY]: (node) => {
+      if(node.data.target.sys.contentType.sys.id === 'step') {
+        return <div>{node.data.target.fields.description}</div>
+      }
+    },
+    [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      return <img src={node.data.target.fields.file.url} alt={node.data.target.fields.description} />
+    },
+    [BLOCKS.EMBEDDED_ENTRY]: (node) => {
+      
+    },
+    //HIPERVINCULOS
+    [INLINES.HYPERLINK]: (node) => {
+      if (node.data.uri.includes("youtube.com")) {
+        // Extract videoId from the URL
+        const match = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/.exec(
+            node.data.uri
+        )
+        const videoId =
+            match && match[7].length === 11 ? match[7] : null
+        return (
+            videoId && (
+              <iframe
+              className="w-full min-h-80 rounded-md"
+              title={`https://youtube.com/embed/${videoId}`}
+              src={`https://youtube.com/embed/${videoId}`}
+              
+              />    
+            )
+        )
+    }
+    }
+  },
+  renderMark: {
+    [MARKS.CODE]: text => {
+      return (
+        <pre className='bg-[#003750] p-7 my-3 rounded-md text-white'>
+          <code>
+            <code className='whitespace-pre-wrap font-monospace text-[14px]'>{text}</code>
+          </code>
+        </pre>
+      );
+    }
+  },
+
+};
 
 function Content({idContent, subtitle}) {
-  const [contentHtml, setContentHtml] = useState('');
 
+  const [json, setJson] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
-    const getContent = async () => {
-      try {
-        const response = await fetch(`/lecciones/Leccion_${idContent}.json`);
-        const json = await response.json();
-        const options = {
-          renderNode: {
-            "embedded-entry-inline": (node) => {
-              const target = node.data.target;
-              if (target.sys.contentType.sys.id === "step") {
-                const description = target.fields.description || "";
-                return documentToHtmlString(description, options);
-              }
-
-              return `<div>Unsupported entry type: ${target.sys.contentType.sys.id}</div>`;
-            },
-            'embedded-entry-block': (node) => {
-            },
-          },
+    if (idContent) {
+      const getContent = async () => {
+        try {
+          const response = await fetch(`/lecciones/Leccion_${idContent}.json`);
+          const data = await response.json();
+          setJson(data);
+          setIsLoading(true);
+        } catch (error) {
+          console.error('Error fetching content:', error);
         }
-        const htmlString = documentToHtmlString(json.data.step.description, options);
-        setContentHtml(htmlString);
-      } catch (error) {
-        console.error('Error fetching content:', error);
-      }
-    };
-    getContent();
+      };
+      getContent();
+    }
   }, [idContent]);
+
+
 
   return (
     <div className='mb-20 px-16 py-6 lg:px-32 lg:py-12 rounded-2xl bg-white'>
       <h1 className='text-2xl font-bold m-0'>{subtitle}</h1>
-      <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <>
+          { isLoading && documentToReactComponents(json.data.step.description, renderOptions)}
+        </>
     </div>
   );
 }
